bba4f531b86633d474cd5fef6636b381adc1d705
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2022-10-02 16:36:39 +0200

    add file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+ const iframe = document.querySelector('#vimeo-player');
+ const player = new Vimeo.Player(iframe);
+
+player.on(
+    'timeupdate',
+    throttle(function (time) {
+        console.log(time.seconds);
+        localStorage.setItem('videoplayer-current-time', `${time.seconds}`);
+    },1000)
+   
+);
+ player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
